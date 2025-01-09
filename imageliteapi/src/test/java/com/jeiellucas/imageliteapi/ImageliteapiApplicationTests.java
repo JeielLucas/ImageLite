066473c1e3d7942b986b05fbc8bd1956b57069a6
Jeiel <jeiellucas0404@gmail.com>
2025-01-09 11:25:42 -0300

    iniciando novo projeto

---
+++ imageliteapi/src/test/java/com/jeiellucas/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeiellucas.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
